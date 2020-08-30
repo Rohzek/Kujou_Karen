6701a690a40166c33e54a33667ce5298fdd57191
Rohzek <rohzek@gmail.com>
2020-08-30 17:59:13 -0500

    Initial commit

---
+++ Kujou_Karen_Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kujou_Karen_Bot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Kujou_Karen
+ Rohzek's Discord Bot, Twitch Bot, and maybe Twitter Bot and more.
